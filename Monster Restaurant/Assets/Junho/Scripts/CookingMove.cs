99275ba5c56cf3f9f0926ab55f4d82aae9fffcc1
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2023-03-14 15:56:33 +0900

    요리

--- Monster Restaurant/Assets/Junho/Scripts/CookingMove.cs
+++ Monster Restaurant/Assets/Junho/Scripts/CookingMove.cs
@@ -12,7 +12,7 @@ public class CookingMove : MonoBehaviour, IPointerDownHandler,IDragHandler
     {
         var x = (eventData.position.x - startPos.x > 0) ? 1: -1;
 
-
+        startPos = eventData.position;
         moveUIs.transform.Translate(Vector2.right * x * moveSpd * Time.deltaTime);
         if (moveUIs.transform.position.x >= 0) moveUIs.transform.position = Vector2.zero;
         else if (moveUIs.transform.position.x <= -16) moveUIs.transform.position = Vector2.right * -16;
