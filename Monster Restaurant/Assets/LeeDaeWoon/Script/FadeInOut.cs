4a10f4ffd1f3c0e07fc72941f36b0b403d2b6c88
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2023-03-14 15:41:51 +0900

    페이드인아웃 추가
    페이드인아웃 추가

---
+++ Monster Restaurant/Assets/LeeDaeWoon/Script/FadeInOut.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour
+{
+
+    void Start()
+    {
+        Fade();
+    }
+
+    void Fade()
+    {
+        Image fade = GetComponent<Image>();
+
+        fade.DOFade(1, 0);
+        fade.DOFade(0, 1).SetEase(Ease.Linear);
+    }
+}
