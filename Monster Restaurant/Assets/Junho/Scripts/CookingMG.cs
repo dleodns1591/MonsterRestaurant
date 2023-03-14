8ea5525918fd75696c15221d8550009952fe0ae9
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2023-03-14 15:39:49 +0900

    주방 마우스 드래그로 이동

---
+++ Monster Restaurant/Assets/Junho/Scripts/CookingMG.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CookingMG : MonoBehaviour
+{
+    public ECookingMainMaterial myMainMaterial;
+    public ECookingSubMaterial mySubMaterial;
+    public ECookingList myCookingList;
+
+    
+}
---
+++ Monster Restaurant/Assets/Junho/Scripts/CookingMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CookingMove : MonoBehaviour, IPointerDownHandler,IDragHandler
+{
+    [SerializeField] private GameObject moveUIs;
+    [SerializeField] private float moveSpd;
+    private Vector2 startPos;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var x = (eventData.position.x - startPos.x > 0) ? 1: -1;
+
+
+        moveUIs.transform.Translate(Vector2.right * x * moveSpd * Time.deltaTime);
+        if (moveUIs.transform.position.x >= 0) moveUIs.transform.position = Vector2.zero;
+        else if (moveUIs.transform.position.x <= -16) moveUIs.transform.position = Vector2.right * -16;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("dd");
+        startPos = eventData.position;
+    }
+   
+}
