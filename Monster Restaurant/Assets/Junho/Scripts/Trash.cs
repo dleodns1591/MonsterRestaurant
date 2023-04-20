f9565ca63584fff1073a9c79c94bb097e33913ba
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2023-04-20 14:16:05 +0900

    Trash

---
+++ Monster Restaurant/Assets/Junho/Scripts/Trash.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+public class Trash : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private float startPos = -800;
+    private float endPos = -223;
+
+    private bool isEnter = false;
+
+    private RectTransform my;
+
+    private void Start()
+    {
+        my = GetComponent<RectTransform>();
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (Cooking.Instance.cookUp == false) return;
+
+        isEnter = true;
+        my.DOAnchorPosY(endPos,0.5f);
+
+        StartCoroutine(DropCheck());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isEnter = false;
+        my.DOAnchorPosY(startPos,0.5f);
+    }
+
+    private IEnumerator DropCheck()
+    {
+        while (isEnter == true)
+        {
+            yield return null;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                print("Drop");
+            }
+        }
+    }
+}
