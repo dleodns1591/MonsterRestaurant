64c50e31f985bb065dfb59c9f57cfaf30587fd70
qkrgustn <sdh210212@sdh.hs.kr>
2023-05-11 14:00:27 +0900

    메모 기능 수정, 푸드클리너 손님 제작중

--- Monster Restaurant/Assets/HS/Scripts/CustomerType/NormalCustomer.cs
+++ Monster Restaurant/Assets/HS/Scripts/CustomerType/NormalCustomer.cs
@@ -34,11 +34,13 @@ public class NormalCustomer : MonoBehaviour, I_CustomerType
         Button cookBtn = cook.transform.parent.GetComponent<Button>();
         Button askBtn = ask.transform.parent.GetComponent<Button>();
 
+        OrderManager.Instance.dialogNumber++;
 
         cook.text = "알겠습니다";
         cookBtn.onClick.AddListener(() =>
         {
             OrderManager.Instance.AskTalk[0] = "알겠습니다";
+            OrderManager.Instance.dialogNumber++;
 
             cookBtn.gameObject.SetActive(false);
             askBtn.gameObject.SetActive(false);
@@ -50,26 +52,33 @@ public class NormalCustomer : MonoBehaviour, I_CustomerType
         askBtn.onClick.AddListener(() =>
         {
             OrderManager.Instance.AskTalk[0] = "네?";
+            OrderManager.Instance.dialogNumber++;
 
             OrderManager.Instance.isNext = true;
+            //손님이 대화하니깐
+            OrderManager.Instance.dialogNumber++;
 
             //시간 깎기
             cookBtn.onClick.RemoveAllListeners();
             cookBtn.onClick.AddListener(() =>
             {
                 OrderManager.Instance.AskTalk[1] = "알겠습니다";
+                OrderManager.Instance.dialogNumber++;
 
                 cookBtn.gameObject.SetActive(false);
                 askBtn.gameObject.SetActive(false);
                 //요리
                 OrderManager.Instance.CookingScene.GetComponent<RectTransform>().DOAnchorPos3DY(0, 1).SetEase(Ease.OutBounce);
             });
-                askBtn.onClick.RemoveAllListeners();
+            askBtn.onClick.RemoveAllListeners();
             askBtn.onClick.AddListener(() =>
             {
                 OrderManager.Instance.AskTalk[1] = "네?";
+                OrderManager.Instance.dialogNumber++;
 
                 OrderManager.Instance.isNext = true;
+                //손님이 대화하니깐
+                OrderManager.Instance.dialogNumber++;
 
                 askBtn.gameObject.SetActive(false);
 
