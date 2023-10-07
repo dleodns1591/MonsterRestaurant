d7cf126672c06c079e83f6f7678b189b49ee263f
LeeDaeWoon <88140817+dleodns1591@users.noreply.github.com>
2023-10-07 14:07:52 +0900

    설정 창 수정
    설정 창 수정

--- Monster Restaurant/Assets/LeeDaeWoon/Script/SubMain/SettingSystem.cs
+++ Monster Restaurant/Assets/LeeDaeWoon/Script/SubMain/SettingSystem.cs
@@ -15,21 +15,21 @@ public class SettingSystem : MonoBehaviour
     [SerializeField] Button settingCancleBtn;
     [SerializeField] Button bgmBtn;
     [SerializeField] Button sfxBtn;
-    [SerializeField] Button vibrationBtn;
-    [SerializeField] Button notificationBtn;
+    //[SerializeField] Button vibrationBtn;
+    //[SerializeField] Button notificationBtn;
 
     [SerializeField] Sprite bgmCancle;
     [SerializeField] Sprite sfxCancle;
-    [SerializeField] Sprite vibarationCancle;
-    [SerializeField] Sprite notificationCancle;
+    //[SerializeField] Sprite vibarationCancle;
+    //[SerializeField] Sprite notificationCancle;
 
     public GameObject amongSetting;
     public GameObject rightSetting;
 
     bool isBGM = false;
     bool isSFX = false;
-    bool isVirbration = false;
-    bool isNotification = false;
+    //bool isVirbration = false;
+    //bool isNotification = false;
 
     [Header("언어")]
     [SerializeField] Button languageBtn;
@@ -38,6 +38,9 @@ public class SettingSystem : MonoBehaviour
     [SerializeField] Button backBtn;
 
     bool isLanguage = true;
+
+    [Header("크레딧")]
+    [SerializeField] Button creditBtn;
 
     void Start()
     {
@@ -54,8 +57,8 @@ public class SettingSystem : MonoBehaviour
     {
         Sprite bgmBasic = bgmBtn.GetComponent<Image>().sprite;
         Sprite sfxBasic = sfxBtn.GetComponent<Image>().sprite;
-        Sprite vibrationBasic = vibrationBtn.GetComponent<Image>().sprite;
-        Sprite notificationBasic = notificationBtn.GetComponent<Image>().sprite;
+        //Sprite vibrationBasic = vibrationBtn.GetComponent<Image>().sprite;
+        //Sprite notificationBasic = notificationBtn.GetComponent<Image>().sprite;
 
         settingCancleBtn.onClick.AddListener(() =>
         {
@@ -108,39 +111,45 @@ public class SettingSystem : MonoBehaviour
         });
 
         // 진동 버튼을 눌렀을 시
-        vibrationBtn.onClick.AddListener(() =>
-        {
-            SoundManager.instance.PlaySoundClip("Button_SFX", SoundType.SFX);
+        //vibrationBtn.onClick.AddListener(() =>
+        //{
+        //    SoundManager.instance.PlaySoundClip("Button_SFX", SoundType.SFX);
 
-            if (!isVirbration)
-            {
-                isVirbration = true;
-                vibrationBtn.GetComponent<Image>().sprite = vibarationCancle;
-            }
+        //    if (!isVirbration)
+        //    {
+        //        isVirbration = true;
+        //        vibrationBtn.GetComponent<Image>().sprite = vibarationCancle;
+        //    }
 
-            else
-            {
-                isVirbration = false;
-                vibrationBtn.GetComponent<Image>().sprite = vibrationBasic;
-            }
-        });
+        //    else
+        //    {
+        //        isVirbration = false;
+        //        vibrationBtn.GetComponent<Image>().sprite = vibrationBasic;
+        //    }
+        //});
 
         // 알림 버튼을 눌렀을 시
-        notificationBtn.onClick.AddListener(() =>
+        //notificationBtn.onClick.AddListener(() =>
+        //{
+        //    SoundManager.instance.PlaySoundClip("Button_SFX", SoundType.SFX);
+
+        //    if (!isNotification)
+        //    {
+        //        isNotification = true;
+        //        notificationBtn.GetComponent<Image>().sprite = notificationCancle;
+        //    }
+
+        //    else
+        //    {
+        //        isNotification = false;
+        //        notificationBtn.GetComponent<Image>().sprite = notificationBasic;
+        //    }
+        //});
+
+        // 크레딧 버튼을 눌렀을 시
+        creditBtn.onClick.AddListener(() =>
         {
             SoundManager.instance.PlaySoundClip("Button_SFX", SoundType.SFX);
-
-            if (!isNotification)
-            {
-                isNotification = true;
-                notificationBtn.GetComponent<Image>().sprite = notificationCancle;
-            }
-
-            else
-            {
-                isNotification = false;
-                notificationBtn.GetComponent<Image>().sprite = notificationBasic;
-            }
         });
     }
 
