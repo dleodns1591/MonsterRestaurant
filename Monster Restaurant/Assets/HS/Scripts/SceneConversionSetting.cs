591cdfc2e1d7d965b3a894188d211246cd87d45b
qkrgustn <sdh210212@sdh.hs.kr>
2023-10-20 20:17:54 +0900

    온라인 배틀 게임 작업중...

--- Monster Restaurant/Assets/HS/Scripts/SceneConversionSetting.cs
+++ Monster Restaurant/Assets/HS/Scripts/SceneConversionSetting.cs
@@ -21,87 +21,91 @@ public class SceneConversionSetting : MonoBehaviour
     {
         GM.ReturnOrder = () =>
         {
-            OM.satisfactionManager.LoopStop();
-
-            string[] line = OM.AnswerTalkTxt.text.Split('\n');
-
-            string[,] SucsessTalk = new string[Enum.GetValues(typeof(EcustomerType)).Length, 3];
-            string[,] FailTalk = new string[Enum.GetValues(typeof(EcustomerType)).Length, 3];
-
-
-            int sucsessCnt = 0;
-            int FailCnt = 0;
-
-            string formerName = "";
-            for (int i = 0; i < line.Length; i++)
+            if(!SaveManager.Instance.isPvp)
             {
-                string[] cell = line[i].Split('\t');
+                OM.satisfactionManager.LoopStop();
 
-                if (formerName != cell[0])
+                string[] line = OM.AnswerTalkTxt.text.Split('\n');
+
+                string[,] SucsessTalk = new string[Enum.GetValues(typeof(EcustomerType)).Length, 3];
+                string[,] FailTalk = new string[Enum.GetValues(typeof(EcustomerType)).Length, 3];
+
+
+                int sucsessCnt = 0;
+                int FailCnt = 0;
+
+                string formerName = "";
+                for (int i = 0; i < line.Length; i++)
                 {
-                    sucsessCnt = 0;
-                    FailCnt = 0;
+                    string[] cell = line[i].Split('\t');
+
+                    if (formerName != cell[0])
+                    {
+                        sucsessCnt = 0;
+                        FailCnt = 0;
+                    }
+
+                    formerName = cell[0];
+                    if (cell[1] == "성공")
+                    {
+                        if (SaveManager.Instance.isEnglish == false)
+                            SucsessTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), sucsessCnt] = cell[2];
+                        else
+                            SucsessTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), sucsessCnt] = cell[3];
+
+                        sucsessCnt++;
+                    }
+                    else if (cell[1] == "실패")
+                    {
+                        if (SaveManager.Instance.isEnglish == false)
+                            FailTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), FailCnt] = cell[2];
+                        else
+                            FailTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), FailCnt] = cell[3];
+
+                        FailCnt++;
+                    }
+
                 }
 
-                formerName = cell[0];
-                if (cell[1] == "성공")
+                if (!OM.isBeggar)
                 {
-                    if (SaveManager.Instance.isEnglish == false)
-                        SucsessTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), sucsessCnt] = cell[2];
+
+                    if (GM.Satisfaction >= 40)
+                        OM.isCookingSuccess = true;
                     else
-                        SucsessTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), sucsessCnt] = cell[3];
+                        OM.isCookingSuccess = false;
 
-                    sucsessCnt++;
-                }
-                else if (cell[1] == "실패")
-                {
-                    if (SaveManager.Instance.isEnglish == false)
-                        FailTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), FailCnt] = cell[2];
+                    //if 성공 실패
+                    if (OM.isCookingSuccess)
+                    {
+                        OM.directingManager.Directing(50, 12);
+                        OM.customer.CustomerImg.sprite = OM.customer.GuestSuccess[OM.NormalGuestType];
+                        OM.AnswerTalk = SucsessTalk[OM.NormalGuestType, UnityEngine.Random.Range(0, 2)];
+                    }
                     else
-                        FailTalk[(int)OM.customerManager.NameToEnumReturn(cell[0]), FailCnt] = cell[3];
-
-                    FailCnt++;
-                }
-
-            }
-
-            if (!OM.isBeggar)
-            {
-
-                if (GM.Satisfaction >= 40)
-                    OM.isCookingSuccess = true;
-                else
-                    OM.isCookingSuccess = false;
-
-                //if 성공 실패
-                if (OM.isCookingSuccess)
-                {
-                    OM.directingManager.Directing(50, 12);
-                    OM.customer.CustomerImg.sprite = OM.customer.GuestSuccess[OM.NormalGuestType];
-                    OM.AnswerTalk = SucsessTalk[OM.NormalGuestType, UnityEngine.Random.Range(0, 2)];
+                    {
+                        OM.directingManager.Directing(20, 4);
+                        int rand = UnityEngine.Random.Range(1, 5);
+                        if (rand == 1)
+                            GM.SettlementCost += 100;
+                        OM.customer.CustomerImg.sprite = OM.customer.GuestFails[OM.NormalGuestType];
+                        OM.AnswerTalk = FailTalk[OM.NormalGuestType, UnityEngine.Random.Range(0, 2)];
+                    }
                 }
                 else
                 {
-                    OM.directingManager.Directing(20, 4);
-                    int rand = UnityEngine.Random.Range(1, 5);
-                    if (rand == 1)
-                        GM.SettlementCost += 100;
-                    OM.customer.CustomerImg.sprite = OM.customer.GuestFails[OM.NormalGuestType];
-                    OM.AnswerTalk = FailTalk[OM.NormalGuestType, UnityEngine.Random.Range(0, 2)];
+                    OM.customer.CustomerImg.sprite = OM.customer.EventGuestSuccess[(int)EeventCustomerType.Beggar];
+                }
+
+                OM.isBeggar = false;
+                GM.isGroupOrder = false;
+                if (!(OM.CustomerType is NormalCustomer))
+                {
+                    OM.customerManager.EeventCustomerSetting(GM.SpecialType);
+                    OM.AnswerTalk = OM.CustomerType.SpecialAnswer();
                 }
             }
-            else
-            {
-                OM.customer.CustomerImg.sprite = OM.customer.EventGuestSuccess[(int)EeventCustomerType.Beggar];
-            }
-
-            OM.isBeggar = false;
-            GM.isGroupOrder = false;
-            if (!(OM.CustomerType is NormalCustomer))
-            {
-                OM.customerManager.EeventCustomerSetting(GM.SpecialType);
-                OM.AnswerTalk = OM.CustomerType.SpecialAnswer();
-            }
+            
 
             OM.CookingScene.transform.DOMoveY(-10, 1).SetEase(Ease.OutBounce).OnComplete(() =>
             {
